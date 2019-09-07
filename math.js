48f09a0d9a16b055a0f110dd15be361eeb6ea8c6
justinaoyov <justina.oyovwevotu@gmail.com>
2019-09-07 18:00:59 +0200

    add function added

--- math.js
+++ math.js
@@ -1,16 +1,19 @@
-const addNumbers = function(a,b) {
-  return a+b;
+const addNumbers = function(a, b) {
+  return a + b;
 };
 
-
-const subtractNumbers = function(a,b) {
-  return a-b;
+const subtractNumbers = function(a, b) {
+  return a - b;
 };
 
+const divideNumbers = function(a, b) {
+  return a / b;
+};
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
 
-console.log("hello");
+console.log(divideNumbers());
 
+console.log("hello");
